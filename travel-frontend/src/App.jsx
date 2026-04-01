07c7c4691b7e973ed5ad5bb60c80ffac1709873f
Shreya Kumari <shreyakumari8523@gmail.com>
2026-04-01 13:03:01 +0530

    Added new routes for ResetPassword, Emergency, and Analytics

--- travel-frontend/src/App.jsx
+++ travel-frontend/src/App.jsx
@@ -1,19 +1,23 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Profile from "./pages/profile";
-import Trips from "./pages/trip";
-/*import ResetPassword from "./pages/ResetPassword";*/
+import Home          from "./pages/Home";
+import Login         from "./pages/Login";
+import Profile       from "./pages/profile";
+import Trips         from "./pages/trip";
+import ResetPassword from "./pages/ResetPassword";
+import Emergency     from "./pages/Emergency";
+import Analytics     from "./pages/Analytics";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/"              element={<Home />} />
-        <Route path="/login"         element={<Login />} />
-        <Route path="/profile"       element={<Profile />} />
-        <Route path="/trips"         element={<Trips />} />
-       
+        <Route path="/"               element={<Home />} />
+        <Route path="/login"          element={<Login />} />
+        <Route path="/profile"        element={<Profile />} />
+        <Route path="/trips"          element={<Trips />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
+        <Route path="/emergency"      element={<Emergency />} />
+        <Route path="/analytics"      element={<Analytics />} />
       </Routes>
     </Router>
   );
